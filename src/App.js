f9d6249e2a638775041c1790dcf5ab9ab011574d
iigunchev <iigunchev@gmail.com>
2022-03-19 16:37:18 +0100

    eslint and prettier config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Cosas</h1>
     </div>
   );
 }
-
 export default App;
